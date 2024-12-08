1d7f72a0e58107d3bc4a3eff5327e1d757340fc2
mateus <fmfr@outlook.com>
2024-12-08 00:17:07 -0300

    primeira versao projeto Terraform

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+droplet_name = "vm-nome-por-variavel"
+# droplet_region=
+# droplet_size=
+# ssh_key_name=
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "droplet_name" {
+  default = "vm-aula"
+}
+
+variable "droplet_region" {
+  default = "nyc1"
+}
+
+variable "droplet_size" {
+  default = "s-1vcpu-2gb"
+}
+
+variable "ssh_key_name" {
+  default = "aula-terraform"
+}
+
+variable "do_token" {
+}
